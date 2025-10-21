4637765669ee6d39c36b65269f2c34d70ec2a0a1
mohanjha2907 <mohanjha0705@gmail.com>
2025-10-21 13:07:13 +0530

    added basic navbar of home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,16 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+
+import type { Metadata } from "next";
+import {Inter} from "next/font/google"
+
+import {NuqsAdapter} from "nuqs/adapters/next/app"
+
+import "./globals.css"
+
+
+const inter=Inter({
+  subsets:["latin"],
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,9 +25,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={inter.className}
       >
-        {children}
+        <NuqsAdapter>
+          {children}
+        </NuqsAdapter>
+        
       </body>
     </html>
   );
